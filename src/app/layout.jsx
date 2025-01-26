fd28603458fa507651247dc99ac04cbcecd5b39f
Sebastian Gonzalez R. <sebagonzalez10@outlook.es>
2025-01-26 13:18:33 -0400

    Agragando GoogleAnalytics 26-01-2025 1:18pm

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,8 +1,11 @@
 //import { Geist, Geist_Mono } from "next/font/google";
+
 import Header from "@/components/Header";
 import "./globals.css";
 import Footer from "@/components/Footer";
 
+
+import { GoogleAnalytics } from '@next/third-parties/google'
 
 export default function RootLayout({ children }) {
   return (
@@ -14,6 +17,7 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
       </body>
+      <GoogleAnalytics gaId="G-VTSHMQ4D54" />
     </html>
   );
 }
