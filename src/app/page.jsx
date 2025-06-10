3e17a24c9f5c56dbbc2feb570bea91a1c403f279
Sebastian Gonzalez R. <sebagonzalez10@outlook.es>
2025-06-10 11:32:41 -0400

    Actualizamos la landing page 10-06-2025 11:32am

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -18,7 +18,7 @@ export default function Home() {
           <p className="md:text-2xl mb-8 text-white">
           En <strong className='text-red-600'>Sevaviajar</strong>, te guiamos en cada paso para que tus trámites de visa, pasaporte y asesorías migratorias sean más sencillos y eficientes. Nos enfocamos en brindarte la información y el apoyo que necesitas para avanzar con confianza hacia tus metas. ¡Comencemos juntos este camino!
           </p>
-          <a href="#servicios" className="px-6 py-3 bg-blue-600 rounded-lg text-white hover:bg-blue-700">
+          <a href="#servicios" className="px-6 py-3 bg-red-600 rounded-lg text-white hover:bg-blue-700">
             Descubre más
           </a>
         </div>
@@ -47,11 +47,11 @@ export default function Home() {
       <ServiciosPrincipales />
 
       {/* Contacto */}
-      <section id="contacto" className="py-16 bg-blue-600 text-white">
+      <section id="contacto" className="py-16 bg-[#024383] text-white">
         <div className="container mx-auto text-center">
           <h3 className="text-3xl font-bold mb-6">Contáctanos</h3>
           <p className="text-lg mb-8">Estamos aquí para ayudarte a cumplir tus sueños de viajar.</p>
-          <a href="mailto:contacto@sevaviajar.com" className="px-6 py-3 bg-red-500 rounded-lg hover:bg-red-600">
+          <a href="mailto:contacto@sevaviajar.com" className="px-6 py-3 bg-red-600 rounded-lg hover:bg-red-600">
             Escríbenos
           </a>
         </div>
