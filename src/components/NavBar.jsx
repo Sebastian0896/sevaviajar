8a7ec310acc62f497b20e280b9b099a1b23849ce
Sebastian Gonzalez R. <sebagonzalez10@outlook.es>
2025-06-10 11:05:15 -0400

    Instalamos el paquete clsx y trabjamos en nuevas caracteristicas del menú, agregamos un nuevo componente llamado NavLink y lo controlamos en NavBar 10-06-2025 11:05am

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,100 +1,20 @@
-import Link from 'next/link';
 import { useState } from 'react';
-import { FiMenu, FiX } from 'react-icons/fi';
+import NavLink from './NavLink';
+
+
+ const links = [
+    { name: 'Acerca', href: '/acerca' },
+    { name: 'Servicios', href: '/servicios'},
+    { name: 'Blog', href: '/blog' },
+    { name: 'Contacto', href: '/contacto' },
+    { name: 'Preguntas Frecuentes', href: '/preguntas-frecuentes' },
+];
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
-
- 
-
   return (
     <nav className="md:w-auto z-50">
-      <div className="container mx-auto flex justify-between items-center py-4 px-6">
-        {/* Logo */}
-        {/* <Link href="/">
-          <span className="text-2xl font-bold text-blue-600 cursor-pointer">Sevaviajar</span>
-        </Link> */}
-
-        {/* Toggle Button for Mobile */}
-        <button
-          className="text-2xl text-gray-700 md:hidden"
-          onClick={toggleMenu}
-          aria-label="Toggle Menu"
-        >
-          {isOpen ? <FiX /> : <FiMenu />}
-        </button>
-
-        {/* Fullscreen Menu */}
-        <div
-          className={`fixed inset-0 md:bg-white bg-slate-900 transition-transform transform ${
-            isOpen ? 'translate-x-0' : 'translate-x-full'
-          } md:static md:translate-x-0 md:flex md:items-end`}
-        >
-          <button
-            className="absolute top-4 right-4 text-2xl text-white md:hidden"
-            onClick={toggleMenu}
-            aria-label="Close Menu"
-          >
-            <FiX />
-          </button>
-
-          <ul className="flex flex-col items-center justify-center h-full space-y-6 md:space-y-0 md:space-x-8 md:flex-row">
-            <li>
-              <Link href="/acerca">
-                <span
-                  className="md:text-[#024383] text-white hover:underline font-medium text-md cursor-pointer"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Acerca
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link href="/servicios">
-                <span
-                  className="md:text-[#024383] text-white hover:underline font-medium text-md cursor-pointer"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Servicios
-                </span>
-              </Link>
-              
-            </li>
-            <li>
-            <Link href="/blog">
-                <span
-                  className="md:text-[#024383] text-white hover:underline font-medium text-md cursor-pointer"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Blog
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link href="/contacto">
-                <span
-                  className="md:text-[#024383] text-white hover:underline font-medium text-md cursor-pointer"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Contacto
-                </span>
-              </Link>
-            </li>
-            <li>
-              <Link href="/preguntas-frecuentes" className='bg-[#024383] p-2 rounded-md hover:border-[#024383] hover:border-solid hover:border-2 hover:bg-transparent transition-colors'>
-                <span
-                  className="text-white md:hover:text-[#024383] font-medium text-md cursor-pointer"
-                  onClick={() => setIsOpen(false)}
-                >
-                  Preguntas Frecuentes
-                </span>
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
+      <NavLink links={links} isOpen={isOpen} setIsOpen={setIsOpen}/>
     </nav>
   );
 }
